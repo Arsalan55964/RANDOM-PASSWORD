126208441d08f34c14cffc700c5f3ba3073aa12e
js-arsalan <oarsalan17@gmail.com>
2025-04-22 00:27:11 +0500

    arrow

---
+++ js/errowfunction.js
@@ -0,0 +1,43 @@
+const user = {
+  username: "arsalan" ,
+  price: "999",
+
+
+  welcomeMessage: function() {
+      console.log(`${this.username} , welcome to website`);
+      console.log(this);
+      
+      
+  }
+}
+
+// user.welcomeMessage()
+// user.username = "sam"
+// user.welcomeMessage()
+
+
+//console.log(this);
+
+// function chai() {
+//     let username = "arsalan"
+//     console.log(this.username) ;
+  
+// }
+// chai()
+
+
+const chai = () => {
+  let username = "arsalan"
+  console.log(this);
+  
+}
+
+
+//chai()
+
+
+// basic arrow function
+
+//  const addTwo = (num1, num2) => {
+//     return num1 + num2
+//  } 
